038facd206a03f00c5277f80d1e0fa073bb6d530
Jakub Firlej <jakub.firlej@outlook.com>
2020-01-25 22:17:53 +0100

    add tasks

--- 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie09.js
+++ 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie09.js
@@ -1,28 +1,25 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var a = document.getElementById("a");
-  var b = document.getElementById("b");
+document.addEventListener('DOMContentLoaded', function() {
+  var a = document.getElementById('a');
+  var b = document.getElementById('b');
 
   // Tutaj this wskazuje na obiekt document (Ponieważ kontext wywołania funkcji
   // addEventListener to właśnie document)
-  console.log("obiekt document: ", this);
+  console.log('obiekt document: ', this);
 
-  a.addEventListener("click", function (event) {
+  a.addEventListener('click', function(event) {
     // Tutaj this wskazuje na element DOM o id a
-    console.log("a: ", this);
+    console.log('a: ', this);
   });
 
-  b.addEventListener("click", function (event) {
+  b.addEventListener('click', function(event) {
     // Tutaj this wskazuje na element DOM o id b
-    console.log("b: ", this);
-
+    console.log('b: ', this);
 
     function innerFuncOne() {
       // Tutaj this wskazuje na element Window, bo funkcja została
       //wywołana bez żadnego kontekstu.
-      console.log("innerFuncOne: ", this);
+      console.log('innerFuncOne: ', this);
     }
     innerFuncOne();
-
   });
-
-})
+});
